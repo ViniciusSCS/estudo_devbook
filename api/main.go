db040530749a3a0da9a188a4756fd49f417b825a
Vinícius Siqueira <vinicius.siqueira@unievangelica.edu.br>
2024-01-10 11:30:44 -0300

    :sparkles: My project

---
+++ api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"api/src/config"
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// func init() {
+// 	chave := make([]byte, 64)
+
+// 	if _, erro := rand.Read(chave); erro != nil {
+// 		log.Fatal(erro)
+// 	}
+
+// 	secret_key := base64.StdEncoding.EncodeToString(chave)
+// 	fmt.Println(secret_key)
+// }
+
+func main() {
+	config.Carregar()
+	r := router.Gerar()
+
+	fmt.Printf("Escutando na porta %d\n", config.Porta)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
+}
